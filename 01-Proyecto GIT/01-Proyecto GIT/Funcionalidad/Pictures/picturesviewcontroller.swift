529016b16c3b5add2c8cab028236375e5f048778
Yoan Tarrillo diaz <yoantd13@gmail.com>
2020-10-26 15:09:04 +0100

    segunda actualización del dia

---
+++ 01-Proyecto GIT/01-Proyecto GIT/Funcionalidad/Pictures/picturesviewcontroller.swift
@@ -0,0 +1,19 @@
+//
+//  picturesviewcontroller.swift
+//  01-Proyecto GIT
+//
+//  Created by Yoan Tarrillo diaz on 26/10/2020.
+//
+
+import Foundation
+import UIKit
+
+
+
+class PictureViewController : UIViewController {
+    let name : String = ""
+    
+    @IBOutlet weak var PhotoViewController: UICollectionView!
+}
+
+
