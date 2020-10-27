616f007f8f24123c777ecad384382353508a3253
Yoan Tarrillo diaz <yoantd13@gmail.com>
2020-10-27 13:01:33 +0100

    extension PictureViewController

---
+++ 01-Proyecto GIT/01-Proyecto GIT/Funcionalidad/Pictures/PicturesCell.swift
@@ -0,0 +1,21 @@
+//
+//  PicturesCell.swift
+//  01-Proyecto GIT
+//
+//  Created by Yoan Tarrillo diaz on 27/10/2020.
+//
+
+import Foundation
+import UIKit
+
+
+
+
+class PicturesCell : UICollectionViewCell {
+    let name : String = ""
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+}
--- 01-Proyecto GIT/01-Proyecto GIT/Funcionalidad/Pictures/picturesviewcontroller.swift
+++ 01-Proyecto GIT/01-Proyecto GIT/Funcionalidad/Pictures/picturesviewcontroller.swift
@@ -11,23 +11,41 @@ import UIKit
 
 
 class PictureViewController : UIViewController {
-    let name : String = ""
     
-    @IBOutlet weak var PhotoViewController: UICollectionView!
+    let reuseIdentifier = String(describing: PicturesCell.self)
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+     override func viewDidLoad() {
+        let nib = UINib(nibName: reuseIdentifier, bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: reuseIdentifier)
+        super.viewDidLoad()
+    }
 }
+
+
 
 extension PictureViewController: UICollectionViewDataSource {
     
-
-func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return 100
-}
-func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
     
-    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CellIdentifier", for: indexPath)
-    cell.backgroundColor = .black
-    return cell
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        cell.backgroundColor = .yellow
+        return cell
+    }
+    
 }
+
+extension PictureViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print (indexPath.row)
+
+    }
+    
 
 }
 
