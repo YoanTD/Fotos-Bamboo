fa47391fae043c99375dd560fbf0a31950af8563
Yoan Tarrillo diaz <yoantd13@gmail.com>
2020-11-05 16:21:59 +0100

    coment

---
+++ 01-Proyecto GIT/01-Proyecto GIT/Funcionalidad/EmailsViewController.swift
@@ -0,0 +1,41 @@
+//
+//  Emails.swift
+//  01-Proyecto GIT
+//
+//  Created by Yoan Tarrillo diaz on 05/11/2020.
+//
+
+import UIKit
+
+class EmailsViewController: UIViewController {
+    
+    
+    @IBOutlet weak var nameOut: UITextField!
+    
+    
+    @IBOutlet weak var emailOut: UITextField!
+    
+    private let nameEmailKey: String = "My nameKey"
+    
+    
+    @IBAction func guardarAct(_ sender: Any) {
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let StoreValue: String = UserDefaults.standard.string(forKey: nameEmailKey) {
+            nameOut.text?.append("\n" + StoreValue)
+            print("Stored value: \(StoreValue)")
+        }
+        
+        
+        
+        guard let nombreTexto: String = nameOut.text else { return }
+        
+        UserDefaults.standard.set(true, forKey: nombreTexto)
+        
+    }
+    
+}
